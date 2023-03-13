72dbae8d2550f269a81f0e3e4552bcede7907ec1
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-03-13 14:29:26 +0200

    Added ObjectPooler Installer;

---
+++ Assets/Scripts/Zenject/Installers/ObjectPoolerInstaller.cs
@@ -0,0 +1,26 @@
+using System;
+using Plugins.ObjectPooler;
+using UnityEngine;
+
+namespace Zenject.Installers
+{
+    public class ObjectPoolerInstaller : MonoInstaller
+    {
+        [Header("References")]
+        [SerializeField] private ObjectPooler _objectPooler;
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            _objectPooler ??= FindObjectOfType<ObjectPooler>();
+        }
+
+        #endregion
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(_objectPooler).AsSingle();
+        }
+    }
+}
